31de3a1bb179fcbe1d538707c56eaebe88f41cbe
DED8IRD <ewu72829@gmail.com>
2018-11-06 21:26:44 -0800

    IO and string concatenation example

---
+++ 1 Python/solutions/hello_you.py
@@ -0,0 +1,10 @@
+# hello_you.py
+first_name = input("Hi, what's your first name: ")
+last_name = input("What's your last name: ")
+
+# Joining strings as arguments in print()
+print('Hello', first_name, last_name)
+# Concatenating using '+'
+print('Hello ' + first_name + ' ' + last_name)
+# Joining using formatting (f-strings)
+print(f'Hello {first_name} {last_name}')
