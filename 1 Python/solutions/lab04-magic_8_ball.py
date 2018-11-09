aa864dd5a09a84a824ad8da59978d1836f1e2817
DED8IRD <ewu72829@gmail.com>
2018-11-08 20:36:28 -0800

    REPL eightball solution

--- 1 Python/solutions/lab04-magic_8_ball.py
+++ 1 Python/solutions/lab04-magic_8_ball.py
@@ -5,6 +5,6 @@ Lab 4: this program simulates a magic 8-ball
 import random
 
 responses = ['Yes', 'Outlook not so good', 'Certainly', 'Without a doubt', 'Ask again later']
-question = input('what is your question? ')
+input('what is your question? ')
 response = random.choice(responses)
 print(response)
